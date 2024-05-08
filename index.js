8ea68f836e60e0cbd02833479724312fbd27d5b5
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-08 19:28:11 +0200

    checked for non numeric characters

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time/3600) //calcultes new distance
+const d2 = d + (vel*time/time) //calcultes new distance
 const rf = fuel - fbr*time //calculates remaining fuel
 const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
 function calcNewVel (acc, vel, time) { 
-  return vel + (acc *3.6) *(time)
+  if (isNaN(acc) || isNaN(vel) || isNaN(time)) {
+    throw Error("!!")
+  }
+  return vel + (acc * 3.6) *(time)
 }
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
