cd4a000afad9785492ec919cd72d58cbcba1e503
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-09 10:09:51 +0200

    named my variables in full for readability and clear understanding

--- index.js
+++ index.js
@@ -6,21 +6,21 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const velocity = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const distance = 0; // distance (km)
+const TemaningFuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const d2 = d + (velocity*time) //calcultes new distance
+const rf = fuelBurnRate*time //calculates remaining fuel
+const vel2 = calcNewVel(acceleration, velocity, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-function calcNewVel (acc, vel, time) { 
-  return vel + (acc * time);
+function calcNewVel (acceleration, velocity, time) { 
+  return velocity + (acceleration * time);
 }
 
  
